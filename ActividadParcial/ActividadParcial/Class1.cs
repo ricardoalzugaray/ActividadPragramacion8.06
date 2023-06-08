f4c7da83a17ccab3f694bf4ad985f50c17a0728a
ricardoalzugaray <ricardoalzugaray@gmail.com>
2023-06-08 09:22:27 -0300

    #1

---
+++ ActividadParcial/ActividadParcial/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActividadParcial
+{
+    internal class Promedio
+    {
+        int valor;
+        int contador = 0;
+        int acumulador = 0;
+        double promedio;
+        public void AgregarValor(int valor)
+        {
+            Console.WriteLine("Ingrese Valor");
+            valor = Convert.ToInt32(Console.ReadLine());
+            acumulador += valor;
+            contador++;
+        }
+        public double CalcularPromedio(int acumulador,int contador)
+        {
+            double promedio = 0;
+            if (contador != 0)
+            {
+                return promedio = acumulador / contador;
+            }
+            else
+            {
+                return 0;
+            }
+            
+        }
+    }
+}
--- ActividadParcial/ActividadParcial/Program.cs
+++ ActividadParcial/ActividadParcial/Program.cs
@@ -10,6 +10,40 @@ namespace ActividadParcial
     {
         static void Main(string[] args)
         {
+            Promedio miPromedio = new Promedio();
+            int opcion, acumulador = 0, contador = 0;
+            do
+            {
+                Menu();
+                opcion = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+                switch (opcion)
+                {
+                    case 1:
+                        {
+                            miPromedio.AgregarValor();
+                            Console.ReadKey();
+                            Console.Clear();
+                        } break;
+                    case 2:
+                        { 
+                            miPromedio.CalcularPromedio(acumulador,contador);
+                            Console.ReadKey();
+                            Console.Clear();
+
+                        } break;
+                    case 3:
+                        {
+
+                        } break;
+                }
+            } while (opcion != 3);
+            void Menu()
+            {
+                Console.WriteLine("1- Agregar Valor");
+                Console.WriteLine("2- Promedio");
+                Console.WriteLine("3- Salir");
+            }
         }
     }
 }
